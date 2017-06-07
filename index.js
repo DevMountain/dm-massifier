b010060a624c3cf8511bb2e9ed85785a718bdd05
Brett Caudill <brett.caudillo@gmail.com>
2017-06-07 12:06:07 -0600

    Middleware changes.

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
 const massive = require('massive');
 const EventEmitter = require('events');
+const DB_READY = 'dbReady';
+
+const dbEvents = new EventEmitter();
 
 let db;
 
+let massifier = {};
+
+massifier.set = function(_db) {
+  db = _db;
+}
+
+massifier.middleware = function() {
+  return function(req, res, next) {
+    req.db = db;
+    next();
+  }
+}
+
+massifier.init = function(cb) {
+  if (!db) {
+    dbEvents.on(DB_READY, () => {
+      cb(db);
+    })
+  } else {
+    cb(db);
+  }
+}
+
+
+massifier.get = function() {
+  return new Promise((resolve, reject) => {
+    if (!db) {
+      dbEvents.on(DB_READY, () => {
+        resolve(db);
+      })
+    } else {
+      resolve(db);
+    }
+  })
+}
 
 module.exports = function(connectionString) {
-  let db;
+  massive(connectionString).then(_db => {
+    db = _db;
+    dbEvents.emit(DB_READY);
+  })
 
-  const dbEmitter = new EventEmitter()
-  massive(connectionString)
-    .then(_db => {
-      db = _db
-      dbEmitter.emit('dbReady')
-    })
-    .catch(err => {
-      throw err
-    })
-
-  return {
-    get: function(cb) {
-      return new Promise((resolve, reject) => {
-        if (!db) {
-          dbEmitter.on('dbReady', () => {
-            if (typeof cb === 'function') {
-              return cb(db);
-            }
-            resolve(db);
-          })
-        } else {
-          if (typeof cb === 'function') {
-            return cb(db);
-          }
-          resolve(db);
-        }
-      })
-    }
-  }
+  return massifier
 }
